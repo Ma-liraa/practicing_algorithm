ee31f55cdee8f6155c0d45bbc043f03ea5caa8a9
Mohammad-ali <maliravi.dev@gmail.com>
2025-11-19 15:37:01 +0330

    change name of interative fn

--- iterative.js
+++ iterative.js
@@ -1,9 +1,9 @@
-const iterativFn = (n) => {
+const iterativeFn = (n) => {
   if (n === 1) return 0;
   if (n === 2) return 1;
-  return iterativFn(n - 1) + iterativFn(n - 2);
+  return iterativeFn(n - 1) + iterativeFn(n - 2);
 };
 
-const n = 5; // 5 => 1 + 2 = 3
-const result = iterativFn(n);
+const n = 5;
+const result = iterativeFn(n);
 console.log(result);
